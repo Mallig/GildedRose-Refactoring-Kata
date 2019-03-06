6f996a2326aa40714533fa9c4ae8c0519d1d1f71
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-03-06 12:31:35 +0000

    tests written for reqular items

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,10 +1,42 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
+  var gildedRose, items
 
   it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
+    gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("foo");
   });
 
+  describe("updateQuality", function() {
+    it("should not decrease quality below zero", function() {
+      gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).toBeGreaterThanOrEqual(0)
+    })
+
+    it("should decrease sellIn by 1 day", function() {
+      gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(-1);
+    })
+
+    describe("for regular items", function() {
+      describe("when sellIn value greater than zero", function() {
+        it("should decrease item quality by 1", function() {
+          gildedRose = new Shop([ new Item("foo", 1, 10) ]);
+          items = gildedRose.updateQuality();
+          expect(items[0].quality).toEqual(9);
+        }) 
+      })
+
+      describe("when sellIn value less than or equal to zero", function() {
+        it("should decrease item quality by 2", function() {
+          gildedRose = new Shop([ new Item("foo", 0, 10) ]);
+          items = gildedRose.updateQuality();
+          expect(items[0].quality).toEqual(8)
+        })
+      })
+    })
+  })
 });
