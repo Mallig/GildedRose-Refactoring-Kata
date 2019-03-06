908be30b8995d477fe728eafeaff27be0a85ffc7
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-03-06 15:34:31 +0000

    extract function to update regular items

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -16,31 +16,23 @@ class Shop {
       if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') { continue }
 
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.updateRegular(this.items[i])
       } else {
         this.updateBackstage(this.items[i])
-      }
-
-
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-                this.items[i].quality = this.items[i].quality - 1;
-            }
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
       }
     }
 
     return this.items;
+  }
+
+  updateRegular(item) {
+    if (item.quality > 0) {
+      item.quality -= 1
+    }
+    item.sellIn -= 1
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality -= 1
+    }
   }
 
   updateBackstage(item) {
