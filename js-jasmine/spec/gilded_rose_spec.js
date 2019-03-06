a58e0fca39866618ae9870ab02b0cca656a821a1
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-03-06 12:37:20 +0000

    tests for Sulfuras item

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -12,14 +12,14 @@ describe("Gilded Rose", function() {
     it("should not decrease quality below zero", function() {
       gildedRose = new Shop([ new Item("foo", 0, 0) ]);
       items = gildedRose.updateQuality();
-      expect(items[0].quality).toBeGreaterThanOrEqual(0)
-    })
+      expect(items[0].quality).toBeGreaterThanOrEqual(0);
+    });
 
     it("should decrease sellIn by 1 day", function() {
       gildedRose = new Shop([ new Item("foo", 0, 0) ]);
       items = gildedRose.updateQuality();
       expect(items[0].sellIn).toEqual(-1);
-    })
+    });
 
     describe("for regular items", function() {
       describe("when sellIn value greater than zero", function() {
@@ -27,16 +27,29 @@ describe("Gilded Rose", function() {
           gildedRose = new Shop([ new Item("foo", 1, 10) ]);
           items = gildedRose.updateQuality();
           expect(items[0].quality).toEqual(9);
-        }) 
-      })
+        });
+      });
 
       describe("when sellIn value less than or equal to zero", function() {
         it("should decrease item quality by 2", function() {
           gildedRose = new Shop([ new Item("foo", 0, 10) ]);
           items = gildedRose.updateQuality();
-          expect(items[0].quality).toEqual(8)
-        })
+          expect(items[0].quality).toEqual(8);
+        });
+      });
+    });
+
+    describe("for Sulfuras, Hand of Ragnaros", function() {
+      gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 10) ]);
+      var sulfuras = gildedRose.updateQuality()[0];
+
+      it("should not decrease sellIn value", function() {
+        expect(sulfuras.sellIn).toEqual(0);
+      });
+
+      it("should not decrease quality", function() {
+        expect(sulfuras.quality).toEqual(10);
       })
-    })
-  })
+    });
+  });
 });
